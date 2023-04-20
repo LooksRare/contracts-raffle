625e6247b947f06c51ef40add79f4f5eaef8d9c3
hiroshi <hiroshi@looksrare.org>
2023-04-20 09:48:10 +0100

    fix: Invalid checksum

--- scripts/deployment/Deployment.s.sol
+++ scripts/deployment/Deployment.s.sol
@@ -38,7 +38,7 @@ contract Deployment is Script {
             protocolFeeRecipient = 0x1838De7d4e4e42c8eB7b204A91e28E9fad14F536;
             protocolFeeBp = 500;
         } else if (chainId == 5) {
-            weth = 0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6;
+            weth = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
             deployerPrivateKey = vm.envUint("GOERLI_KEY");
             keyHash = hex"79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15";
             subscriptionId = 1_1238;
