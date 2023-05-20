93dda8af8060e3b6503ed1d1392f822620745b9f
hiroshi <hiroshi@looksrare.org>
2023-05-20 18:55:27 +0100

    build: Raffle 4

--- scripts/simulation/CreateRaffleMainnet.s.sol
+++ scripts/simulation/CreateRaffleMainnet.s.sol
@@ -17,29 +17,29 @@ contract CreateRaffleMainnet is Script, SimulationBase {
         IRaffle raffle = getRaffle(1);
 
         IRaffle.PricingOption[5] memory pricingOptions;
-        pricingOptions[0] = IRaffle.PricingOption({entriesCount: 1, price: 0.00125 ether});
+        pricingOptions[0] = IRaffle.PricingOption({entriesCount: 1, price: 0.0013 ether});
         pricingOptions[1] = IRaffle.PricingOption({entriesCount: 20, price: 0.024 ether});
         pricingOptions[2] = IRaffle.PricingOption({entriesCount: 100, price: 0.11 ether});
         pricingOptions[3] = IRaffle.PricingOption({entriesCount: 500, price: 0.525 ether});
         pricingOptions[4] = IRaffle.PricingOption({entriesCount: 1_000, price: 0.98 ether});
 
         address bayc = 0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D;
-        address beanz = 0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949;
+        address pengu = 0xBd3531dA5CF5857e7CfAA92426877b022e612cf8;
         address looks = 0xf4d2888d29D722226FafA5d9B24F9164c092421E;
 
-        IRaffle.Prize[] memory prizes = new IRaffle.Prize[](12);
+        IRaffle.Prize[] memory prizes = new IRaffle.Prize[](6);
 
         prizes[0].prizeTier = 0;
         prizes[0].prizeType = IRaffle.TokenType.ERC721;
         prizes[0].prizeAddress = bayc;
-        prizes[0].prizeId = 9910;
+        prizes[0].prizeId = 4686;
         prizes[0].prizeAmount = 1;
         prizes[0].winnersCount = 1;
 
-        for (uint256 i = 1; i < 11; ) {
+        for (uint256 i = 1; i < 5; ) {
             prizes[i].prizeTier = 1;
             prizes[i].prizeType = IRaffle.TokenType.ERC721;
-            prizes[i].prizeAddress = beanz;
+            prizes[i].prizeAddress = pengu;
             prizes[i].prizeAmount = 1;
             prizes[i].winnersCount = 1;
 
@@ -47,31 +47,25 @@ contract CreateRaffleMainnet is Script, SimulationBase {
                 ++i;
             }
         }
-        prizes[1].prizeId = 4847;
-        prizes[2].prizeId = 5170;
-        prizes[3].prizeId = 7220;
-        prizes[4].prizeId = 8052;
-        prizes[5].prizeId = 9012;
-        prizes[6].prizeId = 12392;
-        prizes[7].prizeId = 13799;
-        prizes[8].prizeId = 13518;
-        prizes[9].prizeId = 14440;
-        prizes[10].prizeId = 18038;
+        prizes[1].prizeId = 269;
+        prizes[2].prizeId = 6566;
+        prizes[3].prizeId = 6647;
+        prizes[4].prizeId = 4073;
 
-        prizes[11].prizeTier = 2;
-        prizes[11].prizeType = IRaffle.TokenType.ERC20;
-        prizes[11].prizeAddress = address(looks);
-        prizes[11].prizeAmount = 1_000e18;
-        prizes[11].winnersCount = 99;
+        prizes[5].prizeTier = 2;
+        prizes[5].prizeType = IRaffle.TokenType.ERC20;
+        prizes[5].prizeAddress = address(looks);
+        prizes[5].prizeAmount = 500e18;
+        prizes[5].winnersCount = 100;
 
         console2.logBytes(
             abi.encodeCall(
                 IRaffle.createRaffle,
                 (
                     IRaffle.CreateRaffleCalldata({
-                        cutoffTime: uint40(block.timestamp + 40 hours),
+                        cutoffTime: uint40(block.timestamp + 64 hours),
                         isMinimumEntriesFixed: true,
-                        minimumEntries: 69_000,
+                        minimumEntries: 75_000,
                         maximumEntriesPerParticipant: 14_000,
                         protocolFeeBp: 0,
                         feeTokenAddress: address(0),
