1b2f5ab2f7751a5e46d98f1e5f0432c01d729ff6
hiroshi <hiroshi@looksrare.org>
2023-05-23 14:11:51 +0100

    build: New testnet deployments

--- scripts/simulation/SimulationBase.sol
+++ scripts/simulation/SimulationBase.sol
@@ -13,13 +13,13 @@ contract SimulationBase {
     address public constant SEPOLIA_ERC_721 = 0x61AAEcdbe9C2502a72fec63F2Ff510bE1b95DD97;
     address public constant SEPOLIA_ERC_20 = 0xa68c2CaA3D45fa6EBB95aA706c70f49D3356824E;
     address public constant SEPOLIA_ERC_1155 = 0xE29BcBb8145B8A281BaBDd956e1595b1b76ddAfb;
-    address public constant SEPOLIA_RAFFLE = 0xb0C8a1a0569F7302d36e380755f1835C3e59aCB9;
+    address public constant SEPOLIA_RAFFLE = 0xdA1DB8d3577CB26876Db10f178B18737D4502A94;
 
     address public constant GOERLI_ERC_721 = 0x77566D540d1E207dFf8DA205ed78750F9a1e7c55;
     address public constant GOERLI_ERC_721_B = 0x6019EaF9d6004582248b8F6C5b668675Ce6D22fe;
     address public constant GOERLI_ERC_20 = 0x20A5A36ded0E4101C3688CBC405bBAAE58fE9eeC;
     address public constant GOERLI_ERC_1155 = 0x58c3c2547084CC1C94130D6fd750A3877c7Ca5D2;
-    address public constant GOERLI_RAFFLE = 0xC5F7FCde87e30Aa339d3d61B4fe3c1C261f6EEe2;
+    address public constant GOERLI_RAFFLE = 0xD52F721E00341bA2E4094c8448CBD924c891B25A;
 
     address public constant RAFFLE_OWNER = 0xF332533bF5d0aC462DC8511067A8122b4DcE2B57;
 
