5bc614ad708e8960b23ba790119e748ef819e76b
Hiroshi <108636367+0xhiroshi@users.noreply.github.com>
2023-09-21 15:52:55 +0100

    build: Raffle 12 (#98)

--- scripts/simulation/CreateRaffleMainnet.s.sol
+++ scripts/simulation/CreateRaffleMainnet.s.sol
@@ -22,12 +22,12 @@ contract CreateRaffleMainnet is Script, SimulationBase {
         address azuki = 0xED5AF388653567Af2F388E6224dC7C4b3241C544;
         address elementals = 0xB6a37b5d14D502c3Ab0Ae6f3a0E058BC9517786e;
 
-        IRaffleV2.Prize[] memory prizes = new IRaffleV2.Prize[](32);
+        IRaffleV2.Prize[] memory prizes = new IRaffleV2.Prize[](39);
 
         prizes[0].prizeTier = 0;
         prizes[0].prizeType = IRaffleV2.TokenType.ERC721;
         prizes[0].prizeAddress = bayc;
-        prizes[0].prizeId = 8199;
+        prizes[0].prizeId = 4_858;
         prizes[0].prizeAmount = 1;
         prizes[0].winnersCount = 1;
 
@@ -39,11 +39,11 @@ contract CreateRaffleMainnet is Script, SimulationBase {
             prizes[i].winnersCount = 1;
         }
 
-        prizes[1].prizeId = 8256;
-        prizes[2].prizeId = 5559;
-        prizes[3].prizeId = 7167;
+        prizes[1].prizeId = 5_862;
+        prizes[2].prizeId = 6_933;
+        prizes[3].prizeId = 8_644;
 
-        for (uint256 i = 4; i < 32; i++) {
+        for (uint256 i = 4; i < 39; i++) {
             prizes[i].prizeTier = 2;
             prizes[i].prizeType = IRaffleV2.TokenType.ERC721;
             prizes[i].prizeAddress = elementals;
@@ -51,34 +51,41 @@ contract CreateRaffleMainnet is Script, SimulationBase {
             prizes[i].winnersCount = 1;
         }
 
-        prizes[4].prizeId = 17322;
-        prizes[5].prizeId = 1399;
-        prizes[6].prizeId = 5922;
-        prizes[7].prizeId = 19175;
-        prizes[8].prizeId = 1579;
-        prizes[9].prizeId = 10689;
-        prizes[10].prizeId = 12017;
-        prizes[11].prizeId = 15714;
-        prizes[12].prizeId = 19642;
-        prizes[13].prizeId = 14073;
-        prizes[14].prizeId = 16888;
-        prizes[15].prizeId = 15847;
-        prizes[16].prizeId = 6523;
-        prizes[17].prizeId = 3835;
-        prizes[18].prizeId = 14901;
-        prizes[19].prizeId = 18732;
-        prizes[20].prizeId = 12251;
-        prizes[21].prizeId = 6830;
-        prizes[22].prizeId = 2147;
-        prizes[23].prizeId = 18636;
-        prizes[24].prizeId = 11174;
-        prizes[25].prizeId = 18120;
-        prizes[26].prizeId = 884;
-        prizes[27].prizeId = 3560;
-        prizes[28].prizeId = 3588;
-        prizes[29].prizeId = 13985;
-        prizes[30].prizeId = 3437;
-        prizes[31].prizeId = 14333;
+        prizes[4].prizeId = 16556;
+        prizes[5].prizeId = 2808;
+        prizes[6].prizeId = 12185;
+        prizes[7].prizeId = 9244;
+        prizes[8].prizeId = 4;
+        prizes[9].prizeId = 222;
+        prizes[10].prizeId = 4560;
+        prizes[11].prizeId = 9674;
+        prizes[12].prizeId = 16397;
+        prizes[13].prizeId = 4231;
+        prizes[14].prizeId = 18304;
+        prizes[15].prizeId = 2282;
+        prizes[16].prizeId = 17656;
+        prizes[17].prizeId = 10370;
+        prizes[18].prizeId = 11434;
+        prizes[19].prizeId = 17676;
+        prizes[20].prizeId = 4994;
+        prizes[21].prizeId = 6910;
+        prizes[22].prizeId = 9298;
+        prizes[23].prizeId = 10351;
+        prizes[24].prizeId = 2825;
+        prizes[25].prizeId = 12850;
+        prizes[26].prizeId = 19525;
+        prizes[27].prizeId = 15999;
+        prizes[28].prizeId = 3178;
+        prizes[29].prizeId = 7844;
+        prizes[30].prizeId = 1078;
+        prizes[31].prizeId = 3515;
+        prizes[32].prizeId = 9493;
+        prizes[33].prizeId = 10510;
+        prizes[34].prizeId = 11648;
+        prizes[35].prizeId = 7341;
+        prizes[36].prizeId = 13959;
+        prizes[37].prizeId = 3919;
+        prizes[38].prizeId = 19642;
 
         console2.logBytes(
             abi.encodeCall(
@@ -87,7 +94,7 @@ contract CreateRaffleMainnet is Script, SimulationBase {
                     IRaffleV2.CreateRaffleCalldata({
                         cutoffTime: uint40(block.timestamp + 2 days + 2 hours + 30 minutes),
                         isMinimumEntriesFixed: true,
-                        minimumEntries: 57_000,
+                        minimumEntries: 56_000,
                         maximumEntriesPerParticipant: 15_000,
                         protocolFeeBp: 0,
                         feeTokenAddress: address(0),
