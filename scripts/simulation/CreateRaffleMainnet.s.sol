a4b101b439e4b9b62a870a303198958a2ea8aef0
hiroshi <hiroshi@looksrare.org>
2023-11-21 15:38:49 +0000

    build: Raffle 2023-11-21

--- scripts/simulation/CreateRaffleMainnet.s.sol
+++ scripts/simulation/CreateRaffleMainnet.s.sol
@@ -20,8 +20,9 @@ contract CreateRaffleMainnet is Script, SimulationBase {
 
         address azuki = 0xED5AF388653567Af2F388E6224dC7C4b3241C544;
         address elementals = 0xB6a37b5d14D502c3Ab0Ae6f3a0E058BC9517786e;
+        address beanz = 0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949;
 
-        IRaffleV2.Prize[] memory prizes = new IRaffleV2.Prize[](13);
+        IRaffleV2.Prize[] memory prizes = new IRaffleV2.Prize[](33);
 
         for (uint256 i = 0; i <= 2; i++) {
             prizes[i].prizeTier = 0;
@@ -31,9 +32,9 @@ contract CreateRaffleMainnet is Script, SimulationBase {
             prizes[i].winnersCount = 1;
         }
 
-        prizes[0].prizeId = 4519;
-        prizes[1].prizeId = 7491;
-        prizes[2].prizeId = 7428;
+        prizes[0].prizeId = 7256;
+        prizes[1].prizeId = 5105;
+        prizes[2].prizeId = 7819;
 
         for (uint256 i = 3; i <= 12; i++) {
             prizes[i].prizeTier = 1;
@@ -43,16 +44,45 @@ contract CreateRaffleMainnet is Script, SimulationBase {
             prizes[i].winnersCount = 1;
         }
 
-        prizes[3].prizeId = 3274;
-        prizes[4].prizeId = 4628;
-        prizes[5].prizeId = 7453;
-        prizes[6].prizeId = 9060;
-        prizes[7].prizeId = 10082;
-        prizes[8].prizeId = 12298;
-        prizes[9].prizeId = 13676;
-        prizes[10].prizeId = 13845;
-        prizes[11].prizeId = 18187;
-        prizes[12].prizeId = 19124;
+        prizes[3].prizeId = 724;
+        prizes[4].prizeId = 3499;
+        prizes[5].prizeId = 5014;
+        prizes[6].prizeId = 5257;
+        prizes[7].prizeId = 8335;
+        prizes[8].prizeId = 9380;
+        prizes[9].prizeId = 11776;
+        prizes[10].prizeId = 12711;
+        prizes[11].prizeId = 16255;
+        prizes[12].prizeId = 19174;
+
+        for (uint256 i = 13; i <= 32; i++) {
+            prizes[i].prizeTier = 1;
+            prizes[i].prizeType = IRaffleV2.TokenType.ERC721;
+            prizes[i].prizeAddress = beanz;
+            prizes[i].prizeAmount = 1;
+            prizes[i].winnersCount = 1;
+        }
+
+        prizes[13].prizeId = 1070;
+        prizes[14].prizeId = 1655;
+        prizes[15].prizeId = 3822;
+        prizes[16].prizeId = 4728;
+        prizes[17].prizeId = 4781;
+        prizes[18].prizeId = 4797;
+        prizes[19].prizeId = 5487;
+        prizes[20].prizeId = 5625;
+        prizes[21].prizeId = 6318;
+        prizes[22].prizeId = 6750;
+        prizes[23].prizeId = 7161;
+        prizes[24].prizeId = 7353;
+        prizes[25].prizeId = 8439;
+        prizes[26].prizeId = 8887;
+        prizes[27].prizeId = 9471;
+        prizes[28].prizeId = 10794;
+        prizes[29].prizeId = 15018;
+        prizes[30].prizeId = 18000;
+        prizes[31].prizeId = 18062;
+        prizes[32].prizeId = 18173;
 
         console2.logBytes(
             abi.encodeCall(
@@ -61,7 +91,7 @@ contract CreateRaffleMainnet is Script, SimulationBase {
                     IRaffleV2.CreateRaffleCalldata({
                         cutoffTime: uint40(block.timestamp + 3 days + 2 hours + 30 minutes),
                         isMinimumEntriesFixed: true,
-                        minimumEntries: 23_000,
+                        minimumEntries: 32_000,
                         maximumEntriesPerParticipant: 10_000,
                         protocolFeeBp: 500,
                         feeTokenAddress: address(0),
