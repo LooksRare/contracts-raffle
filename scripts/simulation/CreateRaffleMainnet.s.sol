19bbe097c7de3f6c7c4d6a4cbd2ec494f8deb803
Hiroshi <108636367+0xhiroshi@users.noreply.github.com>
2024-01-09 16:12:18 +0000

    build: Raffle 2024-01-09 (#110)

--- scripts/simulation/CreateRaffleMainnet.s.sol
+++ scripts/simulation/CreateRaffleMainnet.s.sol
@@ -18,69 +18,45 @@ contract CreateRaffleMainnet is Script, SimulationBase {
         pricingOptions[2] = IRaffleV2.PricingOption({entriesCount: 500, price: 0.525 ether});
         pricingOptions[3] = IRaffleV2.PricingOption({entriesCount: 1_000, price: 0.98 ether});
 
-        // address azuki = 0xED5AF388653567Af2F388E6224dC7C4b3241C544;
-        // address elementals = 0xB6a37b5d14D502c3Ab0Ae6f3a0E058BC9517786e;
-        // address beanz = 0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949;
-        address pudgy = 0xBd3531dA5CF5857e7CfAA92426877b022e612cf8;
-        address lilPudgy = 0x524cAB2ec69124574082676e6F654a18df49A048;
-        address rods = 0x062E691c2054dE82F28008a8CCC6d7A1c8ce060D;
+        address azuki = 0xED5AF388653567Af2F388E6224dC7C4b3241C544;
+        address elementals = 0xB6a37b5d14D502c3Ab0Ae6f3a0E058BC9517786e;
+        address beanz = 0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949;
+        // address pudgy = 0xBd3531dA5CF5857e7CfAA92426877b022e612cf8;
+        // address lilPudgy = 0x524cAB2ec69124574082676e6F654a18df49A048;
+        // address rods = 0x062E691c2054dE82F28008a8CCC6d7A1c8ce060D;
 
-        IRaffleV2.Prize[] memory prizes = new IRaffleV2.Prize[](21);
+        IRaffleV2.Prize[] memory prizes = new IRaffleV2.Prize[](3);
 
         prizes[0].prizeTier = 0;
         prizes[0].prizeType = IRaffleV2.TokenType.ERC721;
-        prizes[0].prizeAddress = pudgy;
+        prizes[0].prizeAddress = azuki;
         prizes[0].prizeAmount = 1;
         prizes[0].winnersCount = 1;
-        prizes[0].prizeId = 8488;
+        prizes[0].prizeId = 8_647;
 
-        for (uint256 i = 1; i <= 10; i++) {
-            prizes[i].prizeTier = 1;
-            prizes[i].prizeType = IRaffleV2.TokenType.ERC721;
-            prizes[i].prizeAddress = lilPudgy;
-            prizes[i].prizeAmount = 1;
-            prizes[i].winnersCount = 1;
-        }
+        prizes[1].prizeTier = 1;
+        prizes[1].prizeType = IRaffleV2.TokenType.ERC721;
+        prizes[1].prizeAddress = elementals;
+        prizes[1].prizeAmount = 1;
+        prizes[1].winnersCount = 1;
+        prizes[1].prizeId = 12_684;
 
-        prizes[1].prizeId = 994;
-        prizes[2].prizeId = 2418;
-        prizes[3].prizeId = 5476;
-        prizes[4].prizeId = 9381;
-        prizes[5].prizeId = 9558;
-        prizes[6].prizeId = 11928;
-        prizes[7].prizeId = 12023;
-        prizes[8].prizeId = 15455;
-        prizes[9].prizeId = 17612;
-        prizes[10].prizeId = 21105;
-
-        for (uint256 i = 11; i <= 20; i++) {
-            prizes[i].prizeTier = 2;
-            prizes[i].prizeType = IRaffleV2.TokenType.ERC721;
-            prizes[i].prizeAddress = rods;
-            prizes[i].prizeAmount = 1;
-            prizes[i].winnersCount = 1;
-        }
-
-        prizes[11].prizeId = 194;
-        prizes[12].prizeId = 341;
-        prizes[13].prizeId = 2088;
-        prizes[14].prizeId = 2921;
-        prizes[15].prizeId = 4101;
-        prizes[16].prizeId = 5339;
-        prizes[17].prizeId = 5871;
-        prizes[18].prizeId = 7110;
-        prizes[19].prizeId = 7152;
-        prizes[20].prizeId = 7187;
+        prizes[2].prizeTier = 2;
+        prizes[2].prizeType = IRaffleV2.TokenType.ERC721;
+        prizes[2].prizeAddress = beanz;
+        prizes[2].prizeAmount = 1;
+        prizes[2].winnersCount = 1;
+        prizes[2].prizeId = 6_279;
 
         console2.logBytes(
             abi.encodeCall(
                 IRaffleV2.createRaffle,
                 (
                     IRaffleV2.CreateRaffleCalldata({
-                        cutoffTime: uint40(block.timestamp + 3 days + 2 hours + 30 minutes),
+                        cutoffTime: uint40(block.timestamp + 1 days + 30 minutes),
                         isMinimumEntriesFixed: true,
-                        minimumEntries: 33_000,
-                        maximumEntriesPerParticipant: 10_000,
+                        minimumEntries: 9_500,
+                        maximumEntriesPerParticipant: 2_000,
                         protocolFeeBp: 500,
                         feeTokenAddress: address(0),
                         prizes: prizes,
